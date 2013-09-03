aaf1493d16194237bdd244d7fcd6cbf036727db8
Shane Nicholson <shaner1977@gmail.com>
2013-09-03 15:51:27 -0600

    Fixed PaperClip

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,8 +1,9 @@
 class Pin < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "320x240>"}
-
-  validates :description, presence: true
+	attr_accessible :description, :image 
   validates :user_id, presence: true
+
+  has_attached_file :image, styles: { medium: "320x240>"}
+  validates :description, presence: true
   validates_attachment :image, presence: true,
                             content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
                             size: { less_than: 5.megabytes }
